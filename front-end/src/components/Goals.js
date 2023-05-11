07678836ad26893aa51db5e4ea9abff3be00af15
Edviuxas <sutkuse@gmail.com>
2023-05-11 18:40:05 +0300

    Added add new goal form

--- front-end/src/components/Goals.js
+++ front-end/src/components/Goals.js
@@ -1,10 +1,101 @@
-import React from 'react'
+import { Fab, Tooltip, Modal, Box, Typography, Slider, Input, TextField, Button, Grid, Container } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Add } from '@mui/icons-material';
+import styled from '@emotion/styled';
+
+const CreateGoalModal = styled(Modal)({
+    // margin: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+})
 
 function Goals() {
     const { state } = useLocation();
+    const [createGoalModalOpen, setCreateGoalModalOpen] = useState(false);
+    const [goalsList, setGoalsList] = useState([]);
+
+    useEffect(() => {
+        // setGoalsList([{goalName: 'lalal'}])
+    }, []);
     return (
-        <div>{state.email}</div>
+        <>
+            {((goalsList.length !== 0) ? <div>goals list is not empty</div> : <div>goals list is empty</div>)}
+            <Tooltip
+                onClick={(e) => setCreateGoalModalOpen(true)}
+                title="Create new goal"
+                sx={{
+                    position: "fixed",
+                    bottom: 20,
+                    left: 30
+                }}
+            >
+                <Fab color="primary">
+                    <Add>
+
+                    </Add>
+                </Fab>
+            </Tooltip>
+            <CreateGoalModal
+                open={createGoalModalOpen}
+                onClose={(e) => setCreateGoalModalOpen(false)}
+            >
+                <Box m={1} p={2} borderRadius={5} bgcolor={"background.default"}>
+                    <Typography variant="h6" color="gray" textAlign="center">
+                        Create new goal
+                    </Typography>
+                    <Grid
+                        container
+                        spacing={0.5}
+                        p={3}
+                    >
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                size='small'
+                                required
+                                label="Name"
+                                id="name"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker label="Finish by" slotProps={{ textField: { size: 'small', fullWidth: true } }}/>
+                            </LocalizationProvider>
+                        </Grid>                        
+                    </Grid>
+                    <Box p={2}>
+                        <Typography variant="h7" gutterBottom>
+                            Difficulty
+                        </Typography>
+                        <Slider
+                            id="difficulty"
+                            defaultValue={1}
+                            step={1}
+                            min={1}
+                            marks
+                            max={5}
+                            valueLabelDisplay='auto'
+                        />
+                    </Box>
+                    <Box
+                        sx={{ display: 'flex', justifyContent: 'flex-end' }}
+                    >
+                        <Button
+                            variant='contained'
+                            justifyContent='center'
+                            alignItems='center'
+                        >
+                            Save
+                        </Button>
+                    </Box>
+                </Box>
+            </CreateGoalModal>
+        </>
     )
 }
 
