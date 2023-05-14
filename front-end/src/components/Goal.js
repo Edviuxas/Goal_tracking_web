99453915e1b6f457917dbce6d3987114191a3a39
Edviuxas <sutkuse@gmail.com>
2023-05-14 20:59:15 +0300

    added goal cards

--- front-end/src/components/Goal.js
+++ front-end/src/components/Goal.js
@@ -1,12 +1,11 @@
 import { Card, CardContent, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-function Goal(goalInfo) {
+function Goal(props) {
   return (
-    // <div>{goalInfo.name}</div>
-    <Card sx={{height: 300, width: 300}}>
+    <Card sx={{marginTop: 2, height: 300, width: 300}}>
       <CardContent>
-        <Typography variant='h1'>{goalInfo.goalName}</Typography>
+        <Typography>{props.goalInfo.goalName}</Typography>
       </CardContent>
     </Card>
   )
